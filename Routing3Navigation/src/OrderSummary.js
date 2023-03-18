177a47d4a78548a024fac02e1006fc6a37a3d4ec
kumarbitlabs <108260961+kumarbitlabs@users.noreply.github.com>
2023-03-18 11:00:42 +0530

    Delete Routing3Navigation directory

--- Routing3Navigation/src/OrderSummary.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function OrderSummary() {
-  return (
-    <div className="App">
-      <h1>This is Ordersummary page. your order confirmed</h1>
-    </div>
-  );
-}
-export default OrderSummary;
