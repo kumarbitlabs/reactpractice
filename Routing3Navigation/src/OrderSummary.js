32943ae04bb9c670bf615682cce58bebb76b5411
kumarbitlabs <108260961+kumarbitlabs@users.noreply.github.com>
2023-03-18 10:00:34 +0530

    Add files via upload

---
+++ Routing3Navigation/src/OrderSummary.js
@@ -0,0 +1,9 @@
+import React from "react";
+function OrderSummary() {
+  return (
+    <div className="App">
+      <h1>This is Ordersummary page. your order confirmed</h1>
+    </div>
+  );
+}
+export default OrderSummary;
